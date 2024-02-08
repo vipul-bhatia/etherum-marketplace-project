cb73877096a8ecd4f52d2bf84417e8c55a75b36d
vipul-bhatia <vipul0592bhatia@gmail.com>
2024-02-09 02:10:02 +0530

    .

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -25,9 +25,10 @@ export default function Footer() {
             <Link href="/"  legacyBehavior>
               <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Wishlist</a>
               </Link>
-              <Link href="/"  legacyBehavior>
-              <a  className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
-              </Link>
+              
+               
+              <a  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">Connect</a>
+             
             </div>
           </div>
         </nav>
--- components/course/hero/index.js
+++ components/course/hero/index.js
@@ -1,5 +1,5 @@
 
-
+import Image from 'next/image'
 
 export default function Hero({title, description,image}) {
 
@@ -41,7 +41,7 @@ export default function Hero({title, description,image}) {
           </div>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={image} alt={title} />
+          <Image className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={image} alt={title} layout='fill' />
         </div>
       </div>
     </section>
